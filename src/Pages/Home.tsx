b932103c8a27b3f93bb9cf89dc1f7ba80e6688bb
thomasop <tdss33@hotmail.com>
2023-04-14 19:23:59 +0200

    Add: link user in home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Header from "../Components/Header";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <>
       <Header />
+      <main className="home">
+        <section className="home__section">
+          <h1 className="home__h1">Choisissez votre utilisateur</h1>
+          <div className="home__div">
+            <Link className="home__link" to="/dashboard/12">
+              Karl
+            </Link>
+            <Link className="home__link home__link--margin" to="/dashboard/18">
+              Cecilia
+            </Link>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
